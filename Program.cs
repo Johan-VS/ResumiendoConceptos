139617ebeafc6c2f299a6f0297723b7dfb9c14c7
unknown <johanmorales1989@gmail.com>
2025-01-27 21:55:07 -0400

    nuevossssss

--- Program.cs
+++ Program.cs
@@ -55,5 +55,4 @@ numero1 %= 10;
 
 //Imprimir resultados
 Console.WriteLine("El valor de numero 3 es: " + numero1);
-Console.WriteLine("Despues de la subidad");
-Console.WriteLine("subida despues de la subida");
+Console.WriteLine("Nuevo");
